a142c1c3003ff71a69423a0fd08bfab7c247634d
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-07-31 10:30:06 +0500

    Add ServicesSection component and update index.html to include font library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import ServicesSection from "./components/ServicesSection";
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<><Navbar /><HeroSection /></>} />
+					<Route
+						path="/"
+						element={
+							<>
+								<Navbar />
+								<HeroSection />
+								<ServicesSection />
+							</>
+						}
+					/>
 				</Routes>
 			</BrowserRouter>
 		</>
---
+++ frontend/src/components/ServicesSection.tsx
@@ -0,0 +1,57 @@
+const ServicesSection = () => {
+    const services = [
+        {
+            id: 1,
+            heading: "Robust Workflow",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nulla neque omnis dolore explicabo, voluptatem quo."
+        },
+        {
+            id: 2,
+            heading: "Robust Workflow",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nulla neque omnis dolore explicabo, voluptatem quo."
+        },
+        {
+            id: 3,
+            heading: "Robust Workflow",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nulla neque omnis dolore explicabo, voluptatem quo."
+        },
+        {
+            id: 4,
+            heading: "Robust Workflow",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nulla neque omnis dolore explicabo, voluptatem quo."
+        },
+        {
+            id: 5,
+            heading: "Robust Workflow",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nulla neque omnis dolore explicabo, voluptatem quo."
+        },
+        {
+            id: 6,
+            heading: "Robust Workflow",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nulla neque omnis dolore explicabo, voluptatem quo."
+        }
+    ]
+    return (
+        <>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 pb-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-center relative z-10">
+                {
+                    services.map((service) => (
+                        <div key={service.id}>
+                            <button className="w-18 h-18 bg-[#2274ea] rounded-full flex items-center justify-center mb-5">
+                                <span role="img" aria-label="document">📄</span>
+                            </button>
+                            <h1 className="text-[#2b3546] text-2xl md:text-2xl font-bold leading-tight my-2 font-playfair">
+                                <b>{service.heading}</b>
+                            </h1>
+                            <p className="text-[#7d818a] mb-8 max-w-md font-roboto text-lg">
+                                {service.text}
+                            </p>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
+}
+
+export default ServicesSection
